806d0071f2a2e63de824e6cb16417f27415df431
suba <nahian.suba84@gmail.com>
2025-02-21 01:01:42 +0600

    c++

--- A_A_B_Again.cpp
+++ A_A_B_Again.cpp
@@ -4,14 +4,17 @@ int main(){
     int n;
     cin>>n;
     while (n--) {
-        int num;
+       /*int num;
         cin>>num;
         int ans=0;
         ans=num%10;
         num=num/10;
         ans=ans+num;
     cout<<ans<<"\n";
-        
+        */ 
+        string s;
+        cin>>s;
+        cout<<(s[0]-'0')+(s[1]-'0')<<"\n";
     }
     return 0;
 
