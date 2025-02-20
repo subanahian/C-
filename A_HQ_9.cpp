258bc32295a3327737e037d8df69309420065db1
suba <nahian.suba84@gmail.com>
2025-02-20 20:45:51 +0600

    c++

--- A_HQ_9.cpp
+++ A_HQ_9.cpp
@@ -1,5 +1,18 @@
-inclide<iostream>
+#include<iostream>
 using namespace std;
 int main(){
-    
+    string x;
+    cin>>x; 
+    int track=0;
+    for( int i=0;i<x.size();i++){
+    if (x[i]=='H'||x[i]=='Q'||x[i]=='9') {
+   track=1;
+}  }
+if(track==1){
+    cout<<"YES\n";
+}
+else{
+    cout<<"NO\n";
+}
+  return 0;
 }
