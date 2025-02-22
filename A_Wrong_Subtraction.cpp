9fc4374b3ef06167fb8ede0520fb7b339d69c416
suba <nahian.suba84@gmail.com>
2025-02-23 01:49:19 +0600

    c++

--- A_Wrong_Subtraction.cpp
+++ A_Wrong_Subtraction.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
+
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
+int main() {
+  int n, p;
+  cin >> n >> p;
+  while (p--) {
+    if (n % 10 == 0) {
+
+      n = n / 10;
+    } else {
+      n = n - 1;
+    }
     
-    n-1;
+  }
 
-
+cout << n << "\n";
+  return 0;
 }
