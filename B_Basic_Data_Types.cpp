601726cc9569739a93a23f72d99a96dc9154e64c
suba <nahian.suba84@gmail.com>
2025-03-07 00:16:04 +0600

    cp

---
+++ B_Basic_Data_Types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    long long ll;
+    char ch;
+    float f;
+    double d;
+    cin >> i >> ll >> ch >> f >> d;
+    cout << i <<"\n";
+    cout << ll << "\n";
+    cout << ch <<"\n";
+    cout << f << "\n";
+    cout << d <<"\n" ;
+    return 0;
+}
