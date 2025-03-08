12aa89d855d0a311101159267312f25995f6b536
suba <nahian.suba84@gmail.com>
2025-03-08 11:52:49 +0600

    cp

---
+++ H_Two_numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int A,B;
+    cin>>A>>B;
+    cout<<"floor "<<A<<" / "<<B<<" = "<<floor((double)A/B)<<"\n";
+    cout<<"ceil "<<A<<" / "<<B<<" = "<<ceil((double)A/B)<<"\n";
+    cout<<"round "<<A<<" / "<<B<<" = "<<round((double)A/B)<<"\n";
+    return 0;
+}
