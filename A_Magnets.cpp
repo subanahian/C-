614a3563cf8d11da5cc92abb59824f977fed2ca4
suba <nahian.suba84@gmail.com>
2025-03-11 00:59:40 +0600

    cp

--- A_Magnets.cpp
+++ A_Magnets.cpp
@@ -1,8 +1,18 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
-
+int main() {
+    int t;
+    cin >> t;
+    string currentString,previousString;
+    previousString = "";
+    int ans = 0;
+    while (t--){
+        cin>>currentString;
+        if(currentString != previousString) {
+            ans++;
+        }
+        previousString=currentString;
+    }
+    cout <<ans<<"\n";
     return 0;
 }
