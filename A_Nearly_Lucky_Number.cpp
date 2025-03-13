773f2fac78a2c97ccb961aae2997994bb45963fd
suba <nahian.suba84@gmail.com>
2025-03-13 22:14:04 +0600

    cp

--- A_Nearly_Lucky_Number.cpp
+++ A_Nearly_Lucky_Number.cpp
@@ -1,13 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-    long long n;
+int main(){
+    long long n, luckyDigits = 0;
     cin >> n;
-    int count = 0;
-    if (count == 4 || count == 7) {
-        cout << "YES" <<"\n";
-    } else {
-        cout << "NO" << "\n";
+    while (n){
+        if (n % 10 == 4 || n % 10 == 7) {
+            luckyDigits++;
+        }
+        n /= 10;
+    }
+    int isLucky = 1;
+    if (luckyDigits == 0){
+        isLucky = 0;
+    }
+    while (luckyDigits) {
+        if (luckyDigits % 10 != 4 && luckyDigits % 10 != 7){
+            isLucky = 0;
+            break;
+        }
+        luckyDigits /= 10;
+    }
+    if (isLucky == 1) {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
     }
     return 0;
 }
---
+++ T_Sort_Numbers.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int A,B,C;
+    cin>>A>>B>>C;
+    if (A <= B && A <= C) {
+        cout << A << "\n";
+        if (B <= C) {
+            cout << B << "\n" << C << "\n";
+        } else {
+            cout << C << "\n" << B << "\n";
+        }
+    } else if (B <= A && B <= C) {
+        cout << B << "\n";
+        if (A <= C) {
+            cout << A << "\n" << C << "\n";
+        } else {
+            cout << C << "\n" << A << "\n";
+        }
+    } else {
+        cout << C << "\n";
+        if (A <= B) {
+            cout << A << "\n" << B << "\n";
+        } else {
+            cout << B << "\n" << A << "\n";
+        }
+    }
+    cout << "\n";
+    cout << A << "\n" << B << "\n" << C << "\n";
+    return 0;
+}
