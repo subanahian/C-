c908a0e8dedf175ee7ccaade2bebea6701d8ec15
suba <nahian.suba84@gmail.com>
2025-03-14 00:16:42 +0600

    cp

--- A_Stair_Peak_or_Neither.cpp
+++ A_Stair_Peak_or_Neither.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 int main() {
     int t;
-    cin >> t; 
-    while (t--) {
+    cin>>t; 
+    while (t--){
         int a, b, c;
         cin >> a >> b >> c;
         if (a < b && b < c) {
-            cout << "STAIR" <<"\n";
+            cout <<"STAIR"<<"\n";
         }
         else if (a < b && b > c) {
-            cout << "PEAK" <<"\n";
+            cout <<"PEAK"<<"\n";
         }
         else {
-            cout << "NONE" <<"\n";
+            cout<<"NONE"<<"\n";
         }
     }
     return 0;
