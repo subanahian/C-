fff879c27b3841f6e1890b59e694e4d7e1299cda
suba <nahian.suba84@gmail.com>
2025-03-13 22:17:38 +0600

    cp

--- T_Sort_Numbers.cpp
+++ T_Sort_Numbers.cpp
@@ -3,23 +3,23 @@ using namespace std;
 int main() {
     int A,B,C;
     cin>>A>>B>>C;
-    if (A <= B && A <= C) {
+    if (A<= B && A<= C) {
         cout << A << "\n";
-        if (B <= C) {
+        if (B<=C) {
             cout << B << "\n" << C << "\n";
         } else {
             cout << C << "\n" << B << "\n";
         }
-    } else if (B <= A && B <= C) {
+    } else if (B<=A && B<=C) {
         cout << B << "\n";
-        if (A <= C) {
+        if (A<=C) {
             cout << A << "\n" << C << "\n";
         } else {
             cout << C << "\n" << A << "\n";
         }
     } else {
         cout << C << "\n";
-        if (A <= B) {
+        if (A<=B) {
             cout << A << "\n" << B << "\n";
         } else {
             cout << B << "\n" << A << "\n";
