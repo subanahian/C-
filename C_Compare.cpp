46a3d42679a75d514325088f99cbb540adeb69e0
suba <nahian.suba84@gmail.com>
2025-04-06 00:10:57 +0600

    4th

---
+++ C_Compare.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string X, Y;
+    cin >> X >> Y;
+    if (X < Y)
+        cout << X << "\n";
+    else
+        cout << Y << "\n";
+    return 0;
+}
