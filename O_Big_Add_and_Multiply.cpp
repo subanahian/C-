e6079541526e1f0e00206bd4a8552ae299e00224
suba <nahian.suba84@gmail.com>
2025-04-27 23:54:13 +0600

    6

--- O_Big_Add_and_Multiply.cpp
+++ O_Big_Add_and_Multiply.cpp
@@ -5,8 +5,8 @@ int main() {
     long long N;
     cin >> N;
 
-    cout << N + 9999 << endl;
-    cout << N * 9999 << endl;
+    cout << N + 9999 << "\n";
+    cout << N * 9999 << "\n";
 
     return 0;
 }
