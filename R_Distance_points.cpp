38e3bb0ee3e329e6148c137f6e59bcd6473f7cb7
suba <nahian.suba84@gmail.com>
2025-04-30 00:15:38 +0600

    6

---
+++ R_Distance_points.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+int main() {
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    double distance = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+    cout << fixed << setprecision(9) << distance << endl;
+    return 0;
+}
