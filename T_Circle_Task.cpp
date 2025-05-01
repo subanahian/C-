6922ed808160b910ff27745df862bb4b2115fdf9
suba <nahian.suba84@gmail.com>
2025-05-02 00:30:44 +0600

    6

---
+++ T_Circle_Task.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long X, Y, R, N;
+    cin >> X >> Y >> R >> N;
+
+    for (int i = 0; i < N; i++) {
+        long long xi, yi;
+        cin >> xi >> yi;
+        long long dx = xi - X;
+        long long dy = yi - Y;
+        if (dx * dx + dy * dy <= R * R) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
+    return 0;
+}
