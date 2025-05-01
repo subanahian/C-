7f04bbef2f666364f006ee117d39b8f26b869c32
suba <nahian.suba84@gmail.com>
2025-05-01 11:39:34 +0600

    6

---
+++ S_Is_Triangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double A, B, C;
+    cin >> A >> B >> C;
+
+    if (A + B > C && A + C > B && B + C > A) {
+        double s = (A + B + C) / 2;
+        double area = sqrt(s * (s - A) * (s - B) * (s - C));
+        cout << "Valid\n";
+        cout << fixed << setprecision(6) << area << endl;
+    } else {
+        cout << "Invalid\n";
+    }
+
+    return 0;
+}
