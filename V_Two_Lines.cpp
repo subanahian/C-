720844171cd6038a3a4745a191c709fc77040e10
suba <nahian.suba84@gmail.com>
2025-05-04 21:40:52 +0600

    6

---
+++ V_Two_Lines.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X1, Y1, X2, Y2;
+    int X3, Y3, X4, Y4;
+    cin >> X1 >> Y1 >> X2 >> Y2;
+    cin >> X3 >> Y3 >> X4 >> Y4;
+    int dx1 = X2 - X1;
+    int dy1 = Y2 - Y1;
+    int dx2 = X4 - X3;
+    int dy2 = Y4 - Y3;
+    int cross = dx1 * dy2 - dx2 * dy1;
+
+    if (cross == 0) {
+        cout << "YES" << endl; 
+    } else {
+        cout << "NO" << endl;  
+    }
+
+    return 0;
+}
