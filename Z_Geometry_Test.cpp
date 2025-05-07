8e588f213cfb8e848b8b38d30a3bd9fcbcb3de6c
suba <nahian.suba84@gmail.com>
2025-05-08 00:16:03 +0600

    6

--- Z_Geometry_Test.cpp
+++ Z_Geometry_Test.cpp
@@ -1,13 +1,10 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h> 
 using namespace std;
 
 int main() {
     double R, S;
     cin >> R >> S;
-
     double halfDiagonal = (S * sqrt(2)) / 2;
-
     if (R >= halfDiagonal) {
         cout << "Circle" << endl;
     } else if (S >= 2 * R) {
@@ -15,6 +12,5 @@ int main() {
     } else {
         cout << "Complex" << endl;
     }
-
     return 0;
 }
