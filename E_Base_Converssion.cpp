c50ac22d3f815b75f4d386229a5d38b16efc2167
suba <nahian.suba84@gmail.com>
2025-05-12 00:11:04 +0600

    7

---
+++ E_Base_Converssion.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+void printBinary(int n) {
+    if (n == 0) return;
+    printBinary(n / 2);
+    cout << n % 2;
+}
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+        cin >> N;
+
+        if (N == 0)
+            cout << 0;
+        else
+            printBinary(N);
+        
+        cout << endl;
+    }
+
+    return 0;
+}
