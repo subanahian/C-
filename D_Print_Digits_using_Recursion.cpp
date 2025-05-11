542e95d28f53f17a07f0c2c791d52b487a9ad08c
suba <nahian.suba84@gmail.com>
2025-05-11 10:22:48 +0600

    7

---
+++ D_Print_Digits_using_Recursion.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+void printDigits(int n) {
+    if (n < 10) {
+        cout << n << " ";
+    } else {
+        printDigits(n / 10);       
+        cout << n % 10 << " ";     
+    }
+}
+
+int main() {
+    int T;
+    cin >> T; 
+
+    for (int i = 0; i < T; i++) {
+        int N;
+        cin >> N;
+        if (N == 0) {
+            cout << "0";
+        } else {
+            printDigits(N);
+        }
+
+        cout << "\n";  
+    }
+
+    return 0;
+}
