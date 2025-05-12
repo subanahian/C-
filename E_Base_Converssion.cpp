68457a9fd523236bafde5f5b27422bac9d82b441
suba <nahian.suba84@gmail.com>
2025-05-13 02:00:31 +0600

    7

--- E_Base_Converssion.cpp
+++ E_Base_Converssion.cpp
@@ -1,28 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h> 
 using namespace std;
-
 
 void printBinary(int n) {
     if (n == 0) return;
     printBinary(n / 2);
     cout << n % 2;
 }
-
 int main() {
     int T;
     cin >> T;
-
     while (T--) {
         int N;
         cin >> N;
-
         if (N == 0)
             cout << 0;
         else
             printBinary(N);
-        
         cout << endl;
     }
-
     return 0;
 }
---
+++ F_Print_Even_Indices.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int A[1005]; 
+
+void printEvenReverse(int i) {
+    if (i < 0) return; 
+    if (i % 2 == 0) {
+        cout << A[i] << " ";
+    }
+    printEvenReverse(i - 1); 
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> A[i]; 
+    }
+
+    printEvenReverse(N - 1); 
+
+    return 0;
+}
