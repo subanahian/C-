332277763702cbf634b67cf310df2103452e32bd
suba <nahian.suba84@gmail.com>
2025-05-20 00:53:31 +0600

    7

--- L_Summation.cpp
+++ L_Summation.cpp
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int sumArray(int arr[], int index) {
+long long sumArray(long long arr[], int index) {
     if (index == 0) {
         return arr[0];
     } else {
         return arr[index] + sumArray(arr, index - 1);
     }
 }
-
 int main() {
     int N;
     cin >> N; 
-
-    int arr[1000]; 
+    long long arr[1000];  
     for (int i = 0; i < N; i++) {
         cin >> arr[i];
     }
-    int result = sumArray(arr, N - 1);
+    long long result = sumArray(arr, N - 1);
     cout << result << "\n";
     return 0;
 }
