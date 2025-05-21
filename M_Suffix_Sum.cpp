0307ca62c8b2a53a0e53486378557529efb0a324
suba <nahian.suba84@gmail.com>
2025-05-21 22:49:26 +0600

    7

---
+++ M_Suffix_Sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+const int MAX = 100000;
+int A[MAX]; 
+int sumLastM(int index, int m) {
+    if (m == 0) return 0; 
+    return A[index] + sumLastM(index - 1, m - 1);
+}
+int main() {
+    int N, M;
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    int result = sumLastM(N - 1, M);
+    cout << result << endl;
+    return 0;
+}
