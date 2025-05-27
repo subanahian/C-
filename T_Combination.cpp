19bb932722395d59e61073d0e5c17213ef49f71c
suba <nahian.suba84@gmail.com>
2025-05-28 00:28:23 +0600

    7

---
+++ T_Combination.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int nCr(int n, int r) {
+    if (r == 0 || r == n)
+        return 1;
+    return nCr(n - 1, r - 1) + nCr(n - 1, r);
+}
+int main() {
+    int N, R;
+    cin >> N >> R;
+    cout << nCr(N, R) << endl;
+    return 0;
+}
