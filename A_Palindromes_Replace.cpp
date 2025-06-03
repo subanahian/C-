df10f61de71c240ab651d49c8d1a55486cc0692d
suba <nahian.suba84@gmail.com>
2025-06-04 00:29:46 +0600

    8

---
+++ A_Palindromes_Replace.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int n = s.size();
+
+    for (int i = 0; i < n / 2; i++) {
+        int j = n - 1 - i;
+
+        if (s[i] == '?' && s[j] == '?') {
+            s[i] = s[j] = 'a'; 
+        } else if (s[i] == '?') {
+            s[i] = s[j]; 
+        } else if (s[j] == '?') {
+            s[j] = s[i]; 
+        } else if (s[i] != s[j]) {
+            cout << -1 << endl;
+            return 0; 
+        }
+    }
+    if (n % 2 == 1 && s[n / 2] == '?') {
+        s[n / 2] = 'a';
+    }
+    cout << s << endl;
+    return 0;
+}
