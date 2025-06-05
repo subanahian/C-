1ac52c3001b25ea1db1e132f0cd73dd2e7adecc6
suba <nahian.suba84@gmail.com>
2025-06-06 00:28:45 +0600

    8

---
+++ C_Koko_And_The_Transformation.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    long long sumA = 0, sumB = 0;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        sumA += a[i];
+    }
+    map<int, int> need; 
+    for (int i = 0; i < m; ++i) {
+        cin >> b[i];
+        sumB += b[i];
+        need[b[i]]++;
+    }
+    if (sumA != sumB) {
+        cout << "No\n";
+        return 0;
+    }
+    priority_queue<int> pq;
+    for (int i = 0; i < n; ++i)
+        pq.push(a[i]);
+    while (!pq.empty()) {
+        int x = pq.top();
+        pq.pop();
+        if (need[x]) {
+            need[x]--;
+            if (need[x] == 0)
+                need.erase(x);
+        } else {
+            if (x == 1) {
+                cout << "No\n";
+                return 0;
+            }
+            pq.push(x / 2);
+            pq.push(x - x / 2);
+        }
+    }
+
+    if (need.empty()) cout << "Yes\n";
+    else cout << "No\n";
+
+    return 0;
+}
