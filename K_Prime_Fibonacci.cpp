2ebdae3659d1992379bc87ba23437ea7ae9a8998
suba <nahian.suba84@gmail.com>
2025-06-09 16:27:41 +0600

    8

---
+++ K_Prime_Fibonacci.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+bool isPrime(int num) {
+    if (num < 2) return false;
+    for (int i = 2; i * i <= num; ++i)
+        if (num % i == 0) return false;
+    return true;
+}
+int main() {
+    vector<int> fib(51);
+    fib[1] = 0;
+    fib[2] = 1;
+    for (int i = 3; i <= 50; ++i)
+        fib[i] = fib[i - 1] + fib[i - 2];
+    vector<string> result(51);
+    for (int i = 1; i <= 50; ++i)
+        result[i] = isPrime(fib[i]) ? "prime" : "not prime";
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        cout << result[N] << '\n';
+    }
+    return 0;
+}
