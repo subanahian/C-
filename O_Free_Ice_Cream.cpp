462a1edca7d138b931ba1217b654c11543f87ab6
suba <nahian.suba84@gmail.com>
2025-06-12 00:55:41 +0600

    8

---
+++ O_Free_Ice_Cream.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    long long x;
+    cin >> n >> x;
+
+    long long iceCream = x;
+    int distressedKids = 0;
+
+    for (int i = 0; i < n; ++i) {
+        char op;
+        long long d;
+        cin >> op >> d;
+
+        if (op == '+') {
+            iceCream += d;
+        } else {
+            if (iceCream >= d) {
+                iceCream -= d;
+            } else {
+                distressedKids++;
+            }
+        }
+    }
+
+    cout << iceCream << " " << distressedKids << endl;
+    return 0;
+}
