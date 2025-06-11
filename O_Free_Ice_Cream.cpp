5a88c9e9d75f8891402e19ec1de798b32906f39a
suba <nahian.suba84@gmail.com>
2025-06-12 00:57:20 +0600

    8

--- O_Free_Ice_Cream.cpp
+++ O_Free_Ice_Cream.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -25,6 +25,6 @@ int main() {
         }
     }
 
-    cout << iceCream << " " << distressedKids << endl;
+    cout << iceCream << " " << distressedKids << "\n";
     return 0;
 }
