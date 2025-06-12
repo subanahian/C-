b5cb01e1c7e7b0dfb480837d9555b261cb3c102a
suba <nahian.suba84@gmail.com>
2025-06-13 00:16:26 +0600

    8

---
+++ P_Police_Recruits.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int available_officers = 0;
+    int untreated_crimes = 0;
+    for (int i = 0; i < n; ++i) {
+        int event;
+        cin >> event;
+        if (event == -1) {
+            if (available_officers > 0) {
+                available_officers--; 
+            } else {
+                untreated_crimes++;   
+            }
+        } else {
+            available_officers += event; 
+        }
+    }
+    cout << untreated_crimes << endl;
+    return 0;
+}
+
