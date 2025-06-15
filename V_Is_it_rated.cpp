4c02d31a81acf2b0d6929b1a0d911d2edcde6802
suba <nahian.suba84@gmail.com>
2025-06-16 00:12:32 +0600

    8

--- V_Is_it_rated.cpp
+++ V_Is_it_rated.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -17,7 +17,7 @@ int main() {
     }
 
     if (changed) {
-        cout << "rated" << endl;
+        cout << "rated" << "\n";
         return 0;
     }
     for (int i = 1; i < n; i++) {
@@ -27,9 +27,9 @@ int main() {
         }
     }
     if (unordered) {
-        cout << "unrated" << endl;
+        cout << "unrated" << "\n";
     } else {
-        cout << "maybe" << endl;
+        cout << "maybe" << "\n";
     }
 
     return 0;
