78bbf3e7a1ea8d05ed06d694b336c509353c607b
suba <nahian.suba84@gmail.com>
2025-06-17 09:30:40 +0600

    8

---
+++ W_Mahmoud_and_Longest_Uncommon_Subsequence.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    if (a == b)
+        cout << -1 << endl;
+    else
+        cout << max(a.length(), b.length()) << endl;
+
+    return 0;
+}
