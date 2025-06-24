e330ca9bf216ae150fed90efb7d330e8142331df
suba <nahian.suba84@gmail.com>
2025-06-25 00:00:39 +0600

    8

--- B_Arpa_s_hard_exam_and_Mehrdad_s_naive_cheat.cpp
+++ B_Arpa_s_hard_exam_and_Mehrdad_s_naive_cheat.cpp
@@ -8,8 +8,7 @@ int main() {
         cout << 1 << endl;
     } else {
         int cycle[] = {8, 4, 2, 6};
-        cout << cycle[(n - 1) % 4] << endl;
+        cout << cycle[(n - 1) % 4] << "\n";
     }
-
     return 0;
 }
