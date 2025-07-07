05cab8b964d83788903da35d72bc83f84c415a82
suba <nahian.suba84@gmail.com>
2025-07-07 12:09:03 +0600

    9

---
+++ Q_Dreamoon_and_Stairs.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int min_moves = (n + 1) / 2;
+    for (int i = min_moves; i <= n; ++i) {
+        if (i % m == 0) {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << -1 << endl;
+    return 0;
+}
