33894eae5ea39067b67d5cbf02db6f7cec07d87e
suba <nahian.suba84@gmail.com>
2025-07-13 22:23:47 +0600

    9

--- T_Olesya_and_Rodion.cpp
+++ T_Olesya_and_Rodion.cpp
@@ -5,22 +5,21 @@ int main() {
     int n, t;
     cin >> n >> t;
     if (t == 10 && n == 1) {
-        cout << -1 << endl;
+        cout << -1 << "\n";
     }
     else if (t == 10) {
         cout << 1; 
         for (int i = 1; i < n; i++) {
             cout << 0; 
         }
-        cout << endl;
+        cout << "\n";
     }
     else {
         cout << t; 
         for (int i = 1; i < n; i++) {
             cout << 0; 
         }
-        cout << endl;
+        cout << "\n";
     }
-
     return 0;
 }
