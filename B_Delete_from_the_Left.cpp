2afd83784f4c88f34f19190d3039883fbdb2db53
suba <nahian.suba84@gmail.com>
2025-07-15 10:45:33 +0600

    10

---
+++ B_Delete_from_the_Left.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int minMovesToEqual(string s, string t) {
+    int i = s.length() - 1;
+    int j = t.length() - 1;
+    int commonSuffix = 0;
+    while (i >= 0 && j >= 0 && s[i] == t[j]) {
+        commonSuffix++;
+        i--;
+        j--;
+    }
+    return (s.length() - commonSuffix) + (t.length() - commonSuffix);
+}
+int main() {
+    string s, t;
+    cin >> s >> t;
+    cout << minMovesToEqual(s, t) << endl;
+    return 0;
+}
