a9b7725aca9c12cdaec161efb8d2122d4da3ba0f
suba <nahian.suba84@gmail.com>
2025-07-22 09:30:55 +0600

    10

--- G_Smallest_Product.cpp
+++ G_Smallest_Product.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
+#include <cmath>  
 using namespace std;
 
+bool isGreater(long long x, int n, const vector<long long>& a) {
+    double log_product = 0.0;
+    for (auto num : a)
+        log_product += log10((double)num); 
+    double log_xn = n * log10((double)x); 
+    return log_xn > log_product;
+}
 int main() {
     int n;
     cin >> n;
     vector<long long> a(n);
-    double log_product = 0;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i)
         cin >> a[i];
-        log_product += log((double)a[i]);
+    long long low = 1, high = 1e12, ans = -1;
+    while (low <= high) {
+        long long mid = (low + high) / 2;
+
+        if (isGreater(mid, n, a)) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
     }
-    double avg_log = log_product / n;
-    long long x = (long long)floor(exp(avg_log)) + 1;
-    cout << x << "\n";
+    cout << ans << "\n";
     return 0;
 }
