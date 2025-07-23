f8110f91da2df4f1294856b4b6829c63680f67e3
suba <nahian.suba84@gmail.com>
2025-07-23 10:36:06 +0600

    10

---
+++ H_Chewbaсca_and_Number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string x;
+    cin >> x;
+    for (int i = 0; i < x.length(); ++i) {
+        int digit = x[i] - '0';
+        int inverted = 9 - digit;
+        if (i == 0 && digit == 9) continue;
+        if (inverted < digit) {
+            x[i] = inverted + '0';
+        }
+    }
+    cout << x << endl;
+    return 0;
+}
