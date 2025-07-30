a7289ad6be455f22a8b39acc733113f91fe7f145
suba <nahian.suba84@gmail.com>
2025-07-31 00:18:57 +0600

    10

---
+++ L_Two_Squares.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double L, S1, S2;
+    int Q;
+    cin >> L >> S1 >> S2;
+    cin >> Q;
+    
+    double relative_speed = fabs(S1 - S2);
+    
+    for (int i = 0; i < Q; i++) {
+        double qi;
+        cin >> qi;
+        if (qi == L * L) {
+            cout << fixed << setprecision(6) << 0.0 << endl;
+            continue;
+        }
+        double sqrt_qi = sqrt(qi);
+        double t = (L - sqrt_qi) * sqrt(2) / relative_speed;
+        cout << fixed << setprecision(6) << t << endl;
+    }
+
+    return 0;
+}
