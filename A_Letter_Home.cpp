e3373237531b9d06109e7fa4a32d386cf83138c0
suba <nahian.suba84@gmail.com>
2025-08-01 12:36:01 +0600

    10

---
+++ A_Letter_Home.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int s, n;
+        cin >> s>> n ;
+        int num;
+        
+    }
+}
---
+++ N_Katryoshka.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n, m, k;
+    cin >> n >> m >> k;
+    long long total = 0;
+    long long t1 = min(n, min(m, k));
+    n -= t1;
+    m -= t1;
+    k -= t1;
+    total += t1;
+    long long t2 = min(n / 2, k);
+    total += t2;
+    cout << total << endl;
+    return 0;
+}
