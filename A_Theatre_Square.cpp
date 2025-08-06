31ee22a5dce7ee026b1ff32b1843b3df56b0bec1
suba <nahian.suba84@gmail.com>
2025-08-07 01:04:21 +0600

    s

--- A_Theatre_Square.cpp
+++ A_Theatre_Square.cpp
@@ -1,7 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main(){
     int n,m,a;
     cin>>n>>m>>a;
-    
+    int  row = (n + a - 1) / a; 
+    int  col = (m + a - 1) / a;
+    int total = row * col;
+    cout << total << "\n";
 }
