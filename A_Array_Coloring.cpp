5106ee01709ba30211f25953963b625724c553b1
suba <nahian.suba84@gmail.com>
2025-08-10 01:39:09 +0600

    s

--- A_Array_Coloring.cpp
+++ A_Array_Coloring.cpp
@@ -1,12 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;
-    while (n--)
-    {
-        cin>>n;
-
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+            total += a[i];
+        }
+        if (total % 2 == 0) 
+        {cout << "YES\n";}
+        else {
+            cout << "NO\n";}
     }
-    
+    return 0;
 }
