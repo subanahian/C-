10580b8ee5b142a09f718b97a1b0ee15fc09d908
suba <nahian.suba84@gmail.com>
2025-08-15 00:35:10 +0600

    s

--- A_Next_Round.cpp
+++ A_Next_Round.cpp
@@ -1,9 +1,24 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
 int main() {
-    int n, j, p = 0;
-    cin >> n >> j;
-    for (int i = 0; i < n; i++) 
-    
-    return 0
+
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    int score = a[k-1];
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] >= score && a[i] > 0) {
+            count++;
+        }
+    }
+    cout << count << "\n";
+    return 0;
 }
