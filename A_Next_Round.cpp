39d50bacfff81f7baea673c73d8452bfc090d002
suba <nahian.suba84@gmail.com>
2025-08-16 00:48:56 +0600

    s

--- A_Next_Round.cpp
+++ A_Next_Round.cpp
@@ -3,17 +3,14 @@
 using namespace std;
 
 int main() {
-
     int n, k;
     cin >> n >> k;
     vector<int> a(n);
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-
     int score = a[k-1];
     int count = 0;
-
     for (int i = 0; i < n; i++) {
         if (a[i] >= score && a[i] > 0) {
             count++;
