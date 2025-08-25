58d371ffeaffea651f2109f61f648bf0d2b653bd
suba <nahian.suba84@gmail.com>
2025-08-25 16:18:11 +0600

    s

---
+++ A_Boy_or_Girl.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){ 
+    
+    string name;
+    cin>> name;
+    
+    set<char>n;
+
+    for(int i=0; i<name.size(); i++){
+        n.insert (name[i]);
+    }
+    
+    int check = n.size();
+    
+    if (check % 2 == 0)
+        cout << "CHAT WITH HER!" << "\n";
+    else
+        cout << "IGNORE HIM!" << "\n";
+
+}
