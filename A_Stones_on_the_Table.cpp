bedb4a1d58b81c325a2c0777800e99ca4c110ffc
suba <nahian.suba84@gmail.com>
2025-08-30 00:42:57 +0600

    sp

--- A_Stones_on_the_Table.cpp
+++ A_Stones_on_the_Table.cpp
@@ -3,8 +3,16 @@ using namespace std;
 
 int main(){
     int n;
-    cin>> n;
+    cin >> n;
     string s;
-    cin>> s;
-    
+    cin >> s;
+
+    int ans = 0;
+    for (int i = 0; i < n; i++){
+        if (s[i] == s[i +1]){
+        ans++;
+    }
+}
+    cout << ans << "\n";
+    return 0;
 }
