913d83d93a7f6b0c2b45d8b280ff3ef51b5b9dee
suba <nahian.suba84@gmail.com>
2025-09-04 13:09:24 +0600

    s

--- A_I_Wanna_Be_the_Guy.cpp
+++ A_I_Wanna_Be_the_Guy.cpp
@@ -10,13 +10,23 @@ int main(){
     int p;
     cin>> p;
     for(int i=0; i<p ;i++){
-    l.insert(i);
+    int temp;
+    cin >> temp;
+    l.insert(temp);
     }
 
     int q;
     cin>> q;
     for(int j=0; j<q ;j++){
-    l.insert(j);
+    int temp;
+    cin >> temp;
+    l.insert(temp);;
     }
 
+    if(l.size() == n){
+    cout << "I become the guy.\n";
+    }
+    else{
+    cout << "Oh, my keyboard!\n";}
 }
+
