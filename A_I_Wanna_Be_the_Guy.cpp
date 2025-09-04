d07767cdeecee26a46b880d1e22491ee0d049ccd
suba <nahian.suba84@gmail.com>
2025-09-04 11:48:20 +0600

    s

---
+++ A_I_Wanna_Be_the_Guy.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdtr1c++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>> n;
+   
+    set<int>l;
+
+    int p;
+    cin>> p;
+    for(int i=0; i<p ;i++){
+    l.insert(i);
+    }
+
+    int q;
+    cin>> q;
+    for(int j=0; j<q ;j++){
+    l.insert(j);
+    }
+
+}
