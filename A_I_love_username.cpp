9a1e823eb705b951ea355afe303a9942c8168699
suba <nahian.suba84@gmail.com>
2025-09-09 21:16:41 +0600

    s

--- A_I_love_username.cpp
+++ A_I_love_username.cpp
@@ -1,8 +1,42 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cin>> n;
+    cin >> n;
 
+    int ans = 0;
+
+    int maxVal;
+    int minVal;
+
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        cin >> temp;
+
+        if (i == 0)
+        {
+            maxVal = temp;
+            minVal = temp;
+            continue;
+        }
+
+        if (temp > maxVal)
+        {
+            maxVal = temp;
+            ans++;
+        }
+        else if (temp < minVal)
+        {
+            minVal = temp;
+            ans++;
+        }
+    }
+
+    cout << ans << "\n";
+
+    return 0;
 }
