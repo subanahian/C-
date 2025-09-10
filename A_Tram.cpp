b7c3045f0b5f156112e0407b8f917f2929b84dcb
suba <nahian.suba84@gmail.com>
2025-09-10 23:19:49 +0600

    sp

---
+++ A_Tram.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a, b;
+    int current = 0;
+    int capacity = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b;
+        current -= a;
+        current += b;
+        if (current > capacity) {
+            capacity = current; 
+        }
+    }
+    cout << capacity << endl;
+    return 0;
+}
