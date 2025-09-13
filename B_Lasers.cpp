8958eb32a9e0e88bdcc02e3716bf386fba8135f9
suba <nahian.suba84@gmail.com>
2025-09-13 20:58:37 +0600

    c

---
+++ B_Lasers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t; 
+    while(t--) {
+        int n, m;
+        long long x, y;
+        cin >> n >> m >> x >> y;
+        for(int i=0; i<n; i++) {
+            long long temp;
+            cin >> temp;
+        }
+        for(int j=0; j<m; j++) {
+            long long temp;
+            cin >> temp;
+        }
+        cout << (n + m) << "\n";
+    }
+    return 0;
+}
