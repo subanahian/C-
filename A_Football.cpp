4b60327a3ea3882efbfdb216f6caeed575f45b8c
suba <nahian.suba84@gmail.com>
2025-09-16 18:15:48 +0600

    s

---
+++ A_Football.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>> s;
+    int p=1;
+    for (int i =1 ; i<s.size(); i++){
+        if(s[i]== s[i-1]){
+            p++;
+            if(p==7){
+                cout<< "YES" <<"\n";
+               return 0;
+            }
+        }
+        else{
+    p=1;
+}
+    }
+cout<< "NO"<< "\n";
+}
