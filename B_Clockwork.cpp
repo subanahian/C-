f16acd490b9ceecbb3a7d98d04fbb1e2dea0e7d0
suba <nahian.suba84@gmail.com>
2025-09-24 22:43:51 +0600

    sp

--- B_Clockwork.cpp
+++ B_Clockwork.cpp
@@ -9,18 +9,16 @@ int main() {
         cin >> n;
         vector<long long> a(n);
         for (int i = 0; i < n; i++) cin >> a[i];
-
         bool ok = true;
         for (int i = 0; i < n; i++) {
-            long long d = max(i, n - 1 - i); // farthest distance from an end
+            long long d = max(i, n - 1 - i);
             if (a[i] <= 2 * d) {
                 ok = false;
                 break;
             }
         }
-
-        if (ok) cout << "YES\n";
-        else cout << "NO\n";
+        if (ok) cout << "YES"<<"\n";
+        else cout << "NO"<<"\n";
     }
     return 0;
 }
