b86ee920fc17e354612175ba99a8f7f10f6cca14
suba <nahian.suba84@gmail.com>
2025-09-28 15:11:26 +0600

    sp

---
+++ B_Prison_Transfer.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, c;
+    long long t;
+    cin >> n >> t >> c;
+
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    long long ans = 0;
+    int len = 0; 
+    for (int i = 0; i < n; i++) {
+        if (a[i] <= t) {
+            len++;
+        } else {
+            if (len >= c) ans += (len - c + 1);
+            len = 0;
+        }
+    }
+    if (len >= c) ans += (len - c + 1);
+    cout << ans << "\n";
+    return 0;
+}
