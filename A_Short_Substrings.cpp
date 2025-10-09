caf521652148c2bc5caf88e39289bdcb81506052
suba <nahian.suba84@gmail.com>
2025-10-09 22:10:57 +0600

    sp

--- A_Short_Substrings.cpp
+++ A_Short_Substrings.cpp
@@ -1,12 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	int t; 	cin>>t;
+	int t; 
+	cin>>t;
 	while(t--){
-		string s; cin>>s;
+		string s; 
+        cin>>s;
 		for(int i=0; i<s.size(); i+=2){ 
 			cout<<s[i];
 		}
 		cout<<s.back()<<'\n';
 	}
+	return 0;
 }
