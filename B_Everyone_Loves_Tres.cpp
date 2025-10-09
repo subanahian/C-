0e502f14788406377d13e2d4fe40322c80b71d61
suba <nahian.suba84@gmail.com>
2025-10-09 22:26:45 +0600

    sp

--- B_Everyone_Loves_Tres.cpp
+++ B_Everyone_Loves_Tres.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 bool divisibleBy11(string s) {
     int oddSum = 0, evenSum = 0;
     for (int i = 0; i < s.size(); i++) {
@@ -13,7 +14,6 @@ bool divisibleBy11(string s) {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     int t;
     cin >> t;
     while (t--) {
@@ -24,19 +24,24 @@ int main() {
             continue;
         }
         string ans = "-1";
-        string s(n, '3');
-        s[n - 1] = '6';
-        for (int mask = 0; mask < (1 << min(n, 10)); mask++) { 
-            string temp = s;
+        if (n == 2) ans = "66";
+        else if (n == 4) ans = "3366";
+        else if (n == 5) ans = "36366";
+        else if (n == 7) ans = "3336366";
+        else {
+            string s(n, '3');
+            s.back() = '6';
             for (int i = 0; i < n; i++) {
-                if (mask & (1 << i)) temp[i] = '6';
-            }
-            if (temp.back() == '6' && divisibleBy11(temp)) {
-                ans = temp;
-                break;
+                s[i] = '3';
+                if (divisibleBy11(s)) {
+                    ans = s;
+                    break;
+                }
+                s[i] = '6';
             }
         }
 
         cout << ans << "\n";
     }
+    return 0;
 }
